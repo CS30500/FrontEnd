1a00ad04fbb324b4d1204cc53b6971ab1155dac0
Juyoung Lee <leejuyoung0918@gmail.com>
2025-05-14 04:18:22 +0900

    Add : merge flow with origin app & BLE connection

--- app/src/main/java/com/example/smartbottle/water/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/smartbottle/water/presentation/HomeScreen.kt
@@ -8,17 +8,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+// juyoung modify
+import android.util.Log
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import com.example.smartbottle.ble.BleManager
 
 @Composable
 fun HomeScreen(
     onNavigation : ()->Unit
 ){
+    // juyoung modify
+    val context = LocalContext.current
+    val bleManager = remember { BleManager(context) }
+    var bleData by remember { mutableStateOf("") }
+
+    // BLE 데이터 수신 처리
+    bleManager.setNotifyCallback { data ->
+        bleData = data
+        Log.d("BLE", "📡 수신된 BLE 데이터: $data")
+    }
+
+    // BLE 스캔 시작
+    LaunchedEffect(Unit) {
+        bleManager.startScan()
+    }
+    // juyoung modify
+
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(text = "Home Screen")
+        Text(text = "BLE 데이터: $bleData") // 수신 값 출력 (Juyoung Modify)
         Button(
             onClick = onNavigation
         ) {
